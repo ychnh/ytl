80ec3a92fb3d577585e9ce4af33b67cf6732e15c
ychnh <36415513+ychnh@users.noreply.github.com>
2019-11-30 10:40:43 +0900

    Update util.py

--- util.py
+++ util.py
@@ -252,37 +252,3 @@ class Progress():
     def time_remain(self, batch_time, epoch, batch):
         sec_remain = self.batches_remain(epoch, batch) * batch_time
         return datetime.timedelta(seconds=sec_remain)
-
-# Visual
-
-def pyplot_show_grid(images):
-    ''' Displays multiple `images` in a grid shape.
-    * images: a list of numpy/PIL images
-    '''
-    L = len(images)
-    R = math.floor( math.sqrt(L) )
-    C = math.ceil(L/R)
-    print('diplaying',L, 'images:',R,'x',C)
-
-    plt.figure(figsize=(12,12))
-    idx = 0
-    f, axarr = plt.subplots(R,C)
-    for i in range(R):
-        for j in range(C):
-            arrIdx = C*i+j
-            if (arrIdx >= L):
-                break
-            axarr[i,j].imshow(images[R*i+j])
-            idx+=1
-
-    plt.show()
-
-def pyplot_show_list(images):
-    ''' Shows multiple large images in a single column
-    * images: list of numpy/PIL images
-    '''
-    N = len(images)
-    plt.figure(figsize=(10,25))
-    for i,img in enumerate(images):
-        axis = plt.subplot(N,1,i+1)
-        plt.imshow(img)
